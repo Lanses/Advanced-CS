167e95eeab465e9ecb00d25dc8fe42d4c7574754
Lanses <chevpilov@gmail.com>
2019-06-04 16:57:43 +0300

    In the first h/w inserting was'n correct folder

---
+++ Travel Project v1/Event/EventLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+
+    public delegate void HandleEvent(double d);
+
+    public class EventLogic
+    {
+        public void PriceOfFlight(double d)
+        {
+            if (d >= 100 && d <= 300)
+            {
+                d *= 0.5;
+            }
+            else if (d >= 300 && d <= 700)
+            {
+                d *= 0.45;
+            }
+            else if (d >= 700 && d <= 1500)
+            {
+                d *= 0.4;
+            }
+        }
+
+        public event HandleEvent PriceDependingDistanceCalculation;
+
+    }
+}
